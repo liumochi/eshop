d411a08ce7829241e29ec15960393df85f1d9e19
liumochi <m18846089770@163.com>
2017-04-22 22:40:30 +0800

    网上购物商城  后台php CI框架

--- js/index.js
+++ js/index.js
@@ -2,104 +2,132 @@
  * Created by Administrator on 2017/4/17.
  */
 $(function(){
-    //商品的类，因为有n多商品，有共同的属性，所以封装成一个类
-    var Product=function(id,name,price,img,quantity){
-        this.id=id;
-        this.name=name;
-        this.price=price;
-        this.img=img;
-        this.quantity=1;
+    var prodComp = (function(){
+        //商品的类，因为有n多商品，有共同的属性，所以封装成一个类
+        var Product=function(id,name,price,img,quantity){
+            this.id=id;
+            this.name=name;
+            this.price=price;
+            this.img=img;
+            this.quantity=1;
 
-    };
-    //购物车对象
-    var cart={
-        totalAmount:0,
-        totalQuantity:0,
-        productList:[],
-        addCart:function(product){//添加商品到购物车
-            this.productList.push(product);
-            this.totalQuantity+=product.quantity;
-            this.totalAmount+=product.quantity*product.price;
-            productComp.render();
-          },
+        };
+        //购物车对象
+        var cart={
+            totalAmount:0,
+            totalQuantity:0,
+            productList:[],
+            addCart:function(product){//添加商品到购物车
+                this.productList.push(product);
+                this.totalQuantity+=product.quantity;
+                this.totalAmount+=product.quantity*product.price;
+                productComp.render();
+            },
 
-        removeCart:function(){//从购物车移除
-            //console.log(111);
-            this.productList=[];
-            this.totalAmount=0;
-            this.totalQuantity=0;
-            productComp.render();
-        }
-    };
-    //商品相关功能对象
-    var productComp={
-        $loading:$('#loading'),
-        $productList:$('#product-list'),
-        $loadMore:$('#load-more'),
-        $empty:$('#empty'),
-        isLoaded:true,
-        isEnd:false,
-        pageNo:1,
-        init:function(){
-            var _this=this;
+            removeCart:function(){//从购物车移除
+                //console.log(111);
+                this.productList=[];
+                this.totalAmount=0;
+                this.totalQuantity=0;
+                productComp.render();
+            }
+        };
+        //商品相关功能对象
+        var productComp={
+            $loading:$('#loading'),
+            $productList:$('#product-list'),
+            $loadMore:$('#load-more'),
+            $empty:$('#empty'),
+            isLoaded:true,
+            isEnd:false,
+            pageNo:1,
+            init:function(){
+                var _this=this;
 
-            this.loadData();//页面初始化先加载数据
+                this.loadData();//页面初始化先加载数据
 
-            this.$productList.on('click','.btn-add-cart',function(){
+                this.$productList.on('click','.btn-add-cart',function(){
 
-                var product=$(this).parents('.product-item').data('item-data');
-                //console.log($(this).parents('.product-item'));
-                product.quantity=parseInt($(this).prev().val());
-                cart.addCart(product);
-            });
-            this.$loadMore.on('click',function(){
-                _this.loadMore();
-            });
-            this.$empty.on('click',function(){
-                cart.removeCart();
-            });
-        },
-        render:function(){
-            $('#quantity').html(cart.totalQuantity);
-            $('#money').html(cart.totalAmount);
-        },
-        loadData:function(){
-            this.$loading.show();
-            $.get('product/get_products',{page:this.pageNo},function(data){
-                for(var i=0;i<data.products.length;i++){
-                    var products=data.products;
-                    var product=new Product(products[i].prod_id,products[i].prod_name,products[i].prod_price,products[i].prod_img);
-                    // var $product=$('<li class="product-item"><img src="'+product.img+'" alt="">\
-                    //                 <div class="product-info">\
-                    //                 <h3 class="product-name">'+product.name+'</h3>\
-                    //             <strong class="product-price">'+product.price+'</strong>\
-                    //             <input type="text" class="quantity" value="'+product.quantity+'">\
-                    //                 <button class="btn-add-cart">Add</button>\
-                    //                 </div>\
-                    //                 </li>');
-                    // $product.data('item-data',product);
-                    var productHtml=template('product-tpl',product);
-                    var $product=$(productHtml);
-                    $product.data('item-data',product);
-                    this.$productList.append($product);
+                    var product=$(this).parents('.product-item').data('item-data');
+                    //console.log($(this).parents('.product-item'));
+                    product.quantity=parseInt($(this).prev().val());
+                    cart.addCart(product);
+                });
+                this.$loadMore.on('click',function(){
+                    _this.loadMore();
+                });
+                this.$empty.on('click',function(){
+                    cart.removeCart();
+                });
+            },
+            render:function(){
+                $('#quantity').html(cart.totalQuantity);
+                $('#money').html(cart.totalAmount);
+            },
+            loadData:function(option){
+                var param=$.extend({page:this.pageNo},option);
+                this.$loading.show();
+                $.get('product/get_products',param,function(data){
+                    for(var i=0;i<data.products.length;i++){
+                        var products=data.products;
+                        var product=new Product(products[i].prod_id,products[i].prod_name,products[i].prod_price,products[i].prod_img);
+                        // var $product=$('<li class="product-item"><img src="'+product.img+'" alt="">\
+                        //                 <div class="product-info">\
+                        //                 <h3 class="product-name">'+product.name+'</h3>\
+                        //             <strong class="product-price">'+product.price+'</strong>\
+                        //             <input type="text" class="quantity" value="'+product.quantity+'">\
+                        //                 <button class="btn-add-cart">Add</button>\
+                        //                 </div>\
+                        //                 </li>');
+                        // $product.data('item-data',product);
+                        var productHtml=template('product-tpl',product);
+                        var $product=$(productHtml);
+                        $product.data('item-data',product);
+                        this.$productList.append($product);
+                    }
+                    this.$loading.hide();
+                    this.$loadMore.show();
+                    this.isLoaded=true;
+                    this.isEnd=data.isEnd;
+                }.bind(this),'json')
+            },
+            loadMore:function(){
+                if(this.isEnd){
+                    alert('没有数据了！');
+                    return;
                 }
-                this.$loading.hide();
-                this.$loadMore.show();
-                this.isLoaded=true;
-                this.isEnd=data.isEnd;
-            }.bind(this),'json')
-        },
-        loadMore:function(){
-            if(this.isEnd){
-                alert('没有数据了！');
-                return;
+                if(this.isLoaded){
+                    this.pageNo++;
+                    this.isLoaded=false;
+                    this.loadData({
+                        cateId:navComp.categoryId,
+                        tagId:navComp.tagId
+                    });
+                }
+            },
+            clear:function(){
+                this.$productList.empty();
             }
-            if(this.isLoaded){
-                this.pageNo++;
-                this.isLoaded=false;
-                this.loadData();
+        };
+        return productComp;
+    })();
+    prodComp.init();
+//导航功能；
+    var navComp = (function () {
+        var navComp={
+            init:function () {
+                $('#nav .main-menu > a').on('click',function(){
+                    navComp.categoryId = $(this).data('cate');
+                    navComp.tagId = $(this).data('tag');
+                    prodComp.clear();
+                    prodComp.loadData({
+                        cateId:navComp.categoryId,
+                        tagId:navComp.tagId
+                    });
+                });
             }
-        }
-    };
-    productComp.init();
+        };
+        return navComp;
+    })();
+    navComp.init();
 });
